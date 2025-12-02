10cd86929c453c0bb85e34efca8f63183c7a4f7d
ISLAM ELMENSHAWY <prog.islam@gmail.com>
2025-12-02 00:18:41 -0500

    Force rebuild with cache busting comments
    - Added timestamp comments to force Docker rebuild
    - Updated HTML with build timestamp
    - Updated main.jsx with cache busting comment
    - This should force a complete rebuild and test our caching fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Cache busting version: 2025-12-02-05-20
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
